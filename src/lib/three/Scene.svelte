cbdabb6fb8e67d9a94f8fa919690045ab169a7a4
dnhmphrs <dan@aufbau.io>
2026-04-06 22:32:19 +0100

    edit color

--- src/lib/three/Scene.svelte
+++ src/lib/three/Scene.svelte
@@ -165,7 +165,7 @@ const animate = (t) => {
 
         mat.opacity = fade * 0.25;
         const brightness = fade * 0.75;
-        mat.color.setRGB(brightness * 0.76, brightness * 0.76, brightness * 0.2);
+        mat.color.setRGB(brightness * 0.76, brightness * 0.76, brightness * 0.76);
     }
 
     renderer.render(scene, camera);
